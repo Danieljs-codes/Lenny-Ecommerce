ed3b788fa18dae6f0c75e4a8db258fb8944134c2
DanielDartDev <olamideonabiyi@student.oauife.edu.ng>
2023-06-06 11:42:10 +0100

    search page

--- main.js
+++ main.js
@@ -1,27 +1,47 @@
 import './style.css';
 
-const password = document.getElementById('input-password');
-const showPassword = document.getElementById('show-password');
+// const password = document.getElementById('input-password');
+// const showPassword = document.getElementById('show-password');
 
-let hidePassword = true;
+// let hidePassword = true;
 
-password.addEventListener('click', function (e) {});
+// password.addEventListener('click', function (e) {});
 
-showPassword.addEventListener('click', (e) => {
-  e.preventDefault();
-  if (!hidePassword) {
-    password.type = 'text';
-    hidePassword = !hidePassword;
-  } else {
-    password.type = 'password';
-    hidePassword = !hidePassword;
-  }
+// showPassword.addEventListener('click', (e) => {
+//   e.preventDefault();
+//   if (!hidePassword) {
+//     password.type = 'text';
+//     hidePassword = !hidePassword;
+//   } else {
+//     password.type = 'password';
+//     hidePassword = !hidePassword;
+//   }
+// });
+
+// const showForm = document.getElementById('show-form');
+// const form = document.getElementById('form');
+
+// showForm.addEventListener('click', function () {
+//   form.classList.toggle('hidden')
+//   form.classList.toggle('flex')
+// });
+
+const sidebar = document.getElementById('sidebar');
+
+const openSidebar = document.getElementById('open-sidebar');
+const overlay = document.getElementById('overlay');
+const showSidebar = document.getElementById('show-sidebar');
+
+showSidebar.addEventListener('click', function () {
+  sidebar.classList.toggle('hidden');
+  sidebar.classList.toggle('flex');
+  overlay.classList.toggle('hidden');
+  overlay.classList.toggle('flex');
 });
 
-const showForm = document.getElementById('show-form');
-const form = document.getElementById('form');
-
-showForm.addEventListener('click', function () {
-  form.classList.toggle('hidden')
-  form.classList.toggle('flex')
+openSidebar.addEventListener('click', function () {
+  sidebar.classList.toggle('hidden');
+  sidebar.classList.toggle('flex');
+  overlay.classList.toggle('hidden');
+  overlay.classList.toggle('flex');
 });
