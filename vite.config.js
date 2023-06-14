471d12d5a8e369cac3f15afb176da580d6e75993
DanielDartDev <olamideonabiyi@student.oauife.edu.ng>
2023-06-14 22:52:16 +0100

    Trying something out

--- vite.config.js
+++ vite.config.js
@@ -1,12 +1,38 @@
 // vite.config.js
+import fs from 'fs';
+import path from 'path';
+
 export default {
   build: {
     rollupOptions: {
-      input: {
-        main: 'index.html',
-        product: 'product-detail.html',
-        search: 'search.html',
-      },
+      input: getHtmlEntryPoints(),
     },
   },
 };
+
+function getHtmlEntryPoints() {
+  const entryPoints = {};
+  const files = fs.readdirSync(__dirname);
+
+  files.forEach(file => {
+    const ext = path.extname(file);
+    if (ext === '.html') {
+      const name = path.basename(file, ext);
+      entryPoints[name] = file;
+    }
+  });
+
+  return entryPoints;
+}
+
+// export default {
+//   build: {
+//     rollupOptions: {
+//       input: {
+//         main: 'index.html',
+//         product: 'product-detail.html',
+//         search: 'search.html',
+//       },
+//     },
+//   },
+// };
